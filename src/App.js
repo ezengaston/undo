fec950a481f5a46ad0981e2ae0c09a62438054a6
Ezen <82026690+ezengaston@users.noreply.github.com>
2021-11-02 21:58:48 +0000

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from "react";
+import { Formik, Field, Form } from "formik";
+import { useSelector, useDispatch } from "react-redux";
+import { ActionCreators } from "redux-undo";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  todo: yup
+    .string()
+    .matches(/[^\s\\]/, "Please add your todo")
+    .required("Please add your todo"),
+});
 
 function App() {
+  const [disablePast, setDisablePast] = useState(true);
+  const [disableFuture, setDisableFuture] = useState(true);
+
+  const todos = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(check, [todos]);
+
+  function check() {
+    if (todos.past.length > 0) {
+      setDisablePast(false);
+    } else if (todos.past.length <= 1) {
+      setDisablePast(true);
+    }
+
+    if (todos.future.length > 0) {
+      setDisableFuture(false);
+    } else {
+      setDisableFuture(true);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <Formik
+        initialValues={{ todo: "" }}
+        onSubmit={(data, { resetForm }) => {
+          dispatch({ type: "ADD", payload: data.todo });
+          resetForm();
+        }}
+        validateOnBlur={false}
+        validationSchema={validationSchema}
+      >
+        {(props) => (
+          <Form>
+            <Field
+              className="input"
+              type="text"
+              name="todo"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.todo}
+            ></Field>
+            {props.errors.todo && props.touched.todo ? (
+              <div>{props.errors.todo}</div>
+            ) : null}
+          </Form>
+        )}
+      </Formik>
+      {todos.present
+        ? todos.present.map((items) => (
+            <div className="todo" key={items.id}>
+              {items.name}
+            </div>
+          ))
+        : ""}
+      <div className="btn-container">
+        <button
+          disabled={disablePast}
+          onClick={() => dispatch(ActionCreators.undo())}
         >
-          Learn React
-        </a>
-      </header>
+          undo
+        </button>
+        <button
+          disabled={disableFuture}
+          onClick={() => dispatch(ActionCreators.redo())}
+        >
+          redo
+        </button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import undoable from "redux-undo";
+import { v4 as uuidv4 } from "uuid";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD":
+      return [...state, { name: action.payload, id: uuidv4() }];
+    default:
+      return state;
+  }
+};
+
+const store = createStore(undoable(reducer));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
